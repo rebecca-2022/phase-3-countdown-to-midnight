42c5263d59d7fa9dd082957652f9c74d4dc35d20
rebecca njeri <rebecca.njeri@student.moringaschool.com>
2022-10-25 14:07:23 +0300

    complete

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown x
+    while x >= 1
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep x
+    while x >= 1
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    "HAPPY NEW YEAR!"
+    sleep(5)
 end
